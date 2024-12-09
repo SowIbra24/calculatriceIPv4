aefefa358254bc9c78f860103302c9db5eabc3ee
SowIbra24 <ibrahima.sow0608@gmail.com>
2024-12-09 20:33:32 +0100

    harmonisation

--- utils.php
+++ utils.php
@@ -15,7 +15,7 @@ function ft_power(int $nbr, int $pow): int
 	return $res;
 }
 
-function ft_bindec(string $binary_string): int|float 
+function ft_bindec(string $binary_string): int 
 {
 	if (!preg_match('/^-?[01]+$/', $binary_string)) 
 		throw new InvalidArgumentException("La chaîne doit contenir uniquement des caractères binaires (0 et 1).");
@@ -43,86 +43,86 @@ function ft_bindec(string $binary_string): int|float
 
 //echo "La conversion est : " . ft_bindec("-1010");
 
-   /**
-     * @brief Calculates the length of a given string.
-     * 
-     * This function iterates through the string to count the number of characters 
-     * until the null-terminator is reached. It does not rely on built-in functions.
-     * 
-     * @param string $str The input string whose length is to be calculated.
-     * @return int The length of the input string.
-     */
-    function ft_strlen(string $str): int
-    {
-        $length = 0; ///< Initialize length counter.
+/**
+ * @brief Calculates the length of a given string.
+ * 
+ * This function iterates through the string to count the number of characters 
+ * until the null-terminator is reached. It does not rely on built-in functions.
+ * 
+ * @param string $str The input string whose length is to be calculated.
+ * @return int The length of the input string.
+ */
+function ft_strlen(string $str): int
+{
+    $length = 0; 
 
-        // Iterate through the string until no character is found.
-        while (isset($str[$length])) {
-            $length++;
-        }
-
-        return $length;
+    // Iterate through the string until no character is found.
+    while (isset($str[$length])) {
+        $length++;
     }
 
-    /**
-     * @brief Splits a string into an array based on a specified separator.
-     * 
-     * This function traverses the input string and separates it into substrings 
-     * whenever the specified separator character is encountered.
-     * 
-     * @param string $separator The character used as the delimiter.
-     * @param string $str The input string to be split.
-     * @return array An array of substrings obtained by splitting the input string.
-     */
-    function ft_explode(string $separator, string $str): array
-    {
-        $length = ft_strlen($str); 
-        $currentPart = ""; 
-        $result = []; 
-        $i = 0; 
+    return $length;
+}
 
-        while ($i < $length) {
-            if ($str[$i] == $separator) {
-                $result[] = $currentPart;
-                $currentPart = ""; 
-            } else {
-                $currentPart .= $str[$i]; 
-            }
-            $i++;
-        }
+/**
+ * @brief Splits a string into an array based on a specified separator.
+ * 
+ * This function traverses the input string and separates it into substrings 
+ * whenever the specified separator character is encountered.
+ * 
+ * @param string $separator The character used as the delimiter.
+ * @param string $str The input string to be split.
+ * @return array An array of substrings obtained by splitting the input string.
+ */
+function ft_explode(string $separator, string $str): array
+{
+    $length = ft_strlen($str); 
+    $currentPart = ""; 
+    $result = []; 
+    $i = 0; 
 
-        if ($currentPart !== "") {
+    while ($i < $length) {
+        if ($str[$i] == $separator) {
             $result[] = $currentPart;
+            $currentPart = ""; 
+        } else {
+            $currentPart .= $str[$i]; 
         }
-
-        return ($result);
+        $i++;
     }
 
-    /**
-     * @brief Joins the elements of an array into a single string with a specified separator.
-     * 
-     * Combines all the elements of an array into a single string, using the specified separator
-     * between each pair of elements. If the array is empty, an empty string is returned.
-     * 
-     * @param string $separator The delimiter used to separate the array elements.
-     * @param array $elements The array of elements to join into a string.
-     * @return string The resulting string after joining the array elements with the separator.
-     */
-    function ft_implode(string $separator, array $elements): string
-    {
-        $resultString = "";
+    if ($currentPart !== "") {
+        $result[] = $currentPart;
+    }
 
-        if (empty($elements)) {
-            return $resultString;
-        }
+    return ($result);
+}
 
-        $resultString .= $elements[0];
-        $currentIndex = 1;
+/**
+ * @brief Joins the elements of an array into a single string with a specified separator.
+ * 
+ * Combines all the elements of an array into a single string, using the specified separator
+ * between each pair of elements. If the array is empty, an empty string is returned.
+ * 
+ * @param string $separator The delimiter used to separate the array elements.
+ * @param array $elements The array of elements to join into a string.
+ * @return string The resulting string after joining the array elements with the separator.
+ */
+function ft_implode(string $separator, array $elements): string
+{
+    $resultString = "";
 
-        while (isset($elements[$currentIndex])) {
-            $resultString .= $separator . $elements[$currentIndex];
-            $currentIndex++;
-        }
-
+    if (empty($elements)) {
         return $resultString;
     }
+
+    $resultString .= $elements[0];
+    $currentIndex = 1;
+
+    while (isset($elements[$currentIndex])) {
+        $resultString .= $separator . $elements[$currentIndex];
+        $currentIndex++;
+    }
+
+    return $resultString;
+}
